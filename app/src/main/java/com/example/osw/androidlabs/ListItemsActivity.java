27a2e1909abd7878fd57708c2ed2f94f4cc9290c
wang0601 <wang0601@algonquinlive.com>
2018-02-07 18:43:43 -0500

    updates for Lab 2

--- app/src/main/java/com/example/osw/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/osw/androidlabs/ListItemsActivity.java
@@ -10,4 +10,29 @@ public class ListItemsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
 }
--- app/src/main/java/com/example/osw/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/osw/androidlabs/StartActivity.java
@@ -10,4 +10,29 @@ public class StartActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
 }
